77f67f648c344d5c1ab022f4423fca153e2f320f
Yulian Symkanynets <yulian.symkanynets@student.tuke.sk>
2023-04-11 06:10:45 +0200

    Odovzdanie 2

--- src/main/java/sk/tuke/gamestudio/ui/ConsoleUI.java
+++ src/main/java/sk/tuke/gamestudio/ui/ConsoleUI.java
@@ -77,6 +77,7 @@ public class ConsoleUI {
                     }
                 }
 
+
                 Rating rating = new Rating(playerName, "TiltMaze", playerRating, new Date());
                 ratingService.setRating(rating);
                 break;
